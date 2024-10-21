3b4ec55a7849382cee3cd02a967c350185122310
Roxana Danalache <155965264+RoxanaDanalache@users.noreply.github.com>
2024-10-21 15:32:18 +0300

    commit e2e

---
+++ cypress/e2e/login1.cy.js
@@ -0,0 +1,10 @@
+describe("Login and Logout Tests", () => {
+    it("log in and log out succesfully", () => {
+        cy.viewport(1920,1080);
+        cy.login("user888@gmail.com", "1234567890")
+        cy.get("button[type='submit']").click();
+        cy.get('[data-test-id="CircularProgressbarWithChildren"]').click();
+        cy.contains("Log out").click();
+        cy.url().should("include", "/account/login"); 
+    });
+});
---
+++ cypress/e2e/login2.cy.js
@@ -0,0 +1,10 @@
+describe("Login and Logout Tests", () => {
+    it("log in and log out succesfully", () => {
+        cy.viewport(1920,1080);
+        cy.login("testowyqa@qa.team", "QA!automation-1")
+        cy.get("button[type='submit']").click();
+        cy.get('[data-test-id="CircularProgressbarWithChildren"]').click();
+        cy.contains("Log out").click();
+        cy.url().should("include", "/account/login"); 
+    });
+});
